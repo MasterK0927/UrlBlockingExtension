bdf2085e3d855267e1f84273e87631686d8a1dd9
MasterK0927 <btech10189.22@bitmesra.ac.in>
2024-04-14 02:48:46 +0530

    final

--- popup.js
+++ popup.js
@@ -2,6 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
     var openSidePanelButton = document.getElementById("openSidePanel");
     openSidePanelButton.addEventListener("click", function () {
       // Send a message to background script to toggle the sidebar
-      chrome.runtime.sendMessage({ action: "openSidebar" });
+      chrome.runtime.sendMessage({ action: "toggleSidebar" });
     });
   });
+  
+  // Message listener to close the popup
+  chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.action === 'closePopup') {
+      window.close();
+    }
+  });
+  
--- service_worker.js
+++ service_worker.js
@@ -3,20 +3,41 @@ import { adultSiteRules } from "./blokedSites/adultSites.js";
 import { socialMediaRules } from "./blokedSites/socialMedia.js";
 import { illegalSiteRules } from "./blokedSites/illegalSites.js";
 
+
+let isSidePanelOpen = false; //flag for tracking the state of the side panel
+
+// Function  for toggling the side panel
+function toggleSidePanel(activeTab) {
+  // if (isSidePanelOpen) {
+  //   //sending the message to close the sidebar if it is open but this opeion is not given by the chrome API
+  //   chrome.tabs.sendMessage(activeTab.id, { action: "closeSidebar" });
+  // } else {
+  //   chrome.sidePanel.open({ windowId: activeTab.windowId });
+  // }
+  // //updating the state of the sidepanel
+  // isSidePanelOpen = !isSidePanelOpen;
+
+  //as the chrome API does not provide the option to close the sidebar, we are using the below code to close the sidebar
+  chrome.sidePanel.open({ windowId: activeTab.windowId });
+
+  // if the side panel is open, sending the instructions to the popup to close
+  chrome.runtime.sendMessage({ action: "closePopup" });
+}
+
+// Querying the active tab and adding a message listener to handle messages from the content script using the chrome API named "tabs" and "runtime"
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-  // Assign the active tab to a variable
   var activeTab = tabs[0];
 
-  // Add a message listener to handle messages
+  // added a message listener to handle the message from the content script to toggle the sidebar using the chrome API named "runtime" and "tabs" 
   chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.action === 'openSidebar') {
-      // Use the windowId from the active tab
-      chrome.sidePanel.open({ windowId: activeTab.windowId });
+    if (message.action === 'toggleSidebar') {
+      //calling the function to toggle the side panel with the active tab as the argument 
+      toggleSidePanel(activeTab);
     }
   });
 });
 
-
+//functions to inject and deinject the service worker by adding and removing the rules from the service worker using the chrome API named "declarativeNetRequest"
 export function injectServiceWorker(socialMediaChecked, adultChecked, illegalChecked) {
   const rulesToInject = [];
 
@@ -39,6 +60,7 @@ export function injectServiceWorker(socialMediaChecked, adultChecked, illegalChe
   console.log("Service worker started.");
 }
 
+// function to deinject the service worker by removing the rules from the service worker using the chrome API named "declarativeNetRequest" 
 export function deinjectServiceWorker() {
   const removeRuleIds = [];
 
@@ -60,5 +82,3 @@ export function deinjectServiceWorker() {
 
   console.log("Service worker stopped.");
 }
-
-
